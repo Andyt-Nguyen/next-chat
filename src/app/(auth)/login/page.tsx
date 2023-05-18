086cff802805c8d0d426a117fdac9f5d22561d9f
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2023-05-18 10:37:15 -0700

    fix(ReadMe): updated

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import Button from '@/components/ui/button';
 import { FC, useState } from 'react';
 import { signIn } from 'next-auth/react';
 import { toast } from 'react-hot-toast';
+import Image from 'next/image';
 
 interface LoginProps {}
 
@@ -25,8 +26,7 @@ const Login: FC<LoginProps> = () => {
 			<div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
 				<div className="w-full flex flex-col items-center max-w-md space-y-8">
 					<div className="flex flex-col item-center gap-8">
-						logo
-						<h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+						<h2 className="mt-2 text-center text-3xl font-bold tracking-tight text-gray-900">
 							Sign into your account
 						</h2>
 					</div>
